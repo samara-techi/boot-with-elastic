24cccff1c5dfa545352932a473ada7a8f4def70b
samar <kssrgml@gmail.com>
2020-06-14 18:47:30 +0530

    Rest api that generates random car details

---
+++ practical-java/src/main/java/com/course/practicaljava/rest/domain/Car.java
@@ -0,0 +1,37 @@
+package com.course.practicaljava.rest.domain;
+
+public class Car {
+	private String brand;
+	private String color;
+	private String type;
+	
+	public Car() {}
+	public Car(String brand, String color, String type) {
+		this.brand = brand;
+		this.color = color;
+		this.type = type;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	@Override
+	public String toString() {
+		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + "]";
+	}
+}
---
+++ practical-java/src/main/java/com/course/practicaljava/rest/service/CarService.java
@@ -0,0 +1,18 @@
+package com.course.practicaljava.rest.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.course.practicaljava.rest.domain.Car;
+
+
+public interface CarService {
+	
+	List<String> BRANDS = Arrays.asList("Toyota", "Honda", "Ford");
+	List<String> COLORS = Arrays.asList("red", "black", "white");
+	List<String> TYPES = Arrays.asList("Sedan", "SUV", "MPV");
+	
+	Car generateCar();
+
+}
