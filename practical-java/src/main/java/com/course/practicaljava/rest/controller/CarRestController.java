ce2ecd87732f6a0b5c1192a0151d245206012ab1
samar <kssrgml@gmail.com>
2020-06-14 19:28:38 +0530

    Post api to add new Car

--- practical-java/src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
+++ practical-java/src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
@@ -3,6 +3,8 @@ package com.course.practicaljava.rest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,12 @@ public class CarRestController {
 			    produces = MediaType.APPLICATION_JSON_VALUE)
 	public Car random() {
 		return carService.generateCar();
+	}
+	
+	@PostMapping(path="/echo", 
+			     consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String echo(@RequestBody Car car) {
+		return car.toString();
 	}
 
 }
