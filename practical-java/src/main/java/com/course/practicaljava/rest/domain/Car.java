c1d7c39ead38839f72988da96b337ab570365583
samar <kssrgml@gmail.com>
2020-06-15 09:36:25 +0530

    price, availability, release date added in car.java

--- practical-java/src/main/java/com/course/practicaljava/rest/domain/Car.java
+++ practical-java/src/main/java/com/course/practicaljava/rest/domain/Car.java
@@ -1,9 +1,14 @@
 package com.course.practicaljava.rest.domain;
 
+import java.util.Date;
+
 public class Car {
 	private String brand;
 	private String color;
 	private String type;
+	private int price;
+	private boolean available;
+	private Date firstReleaseDate;
 	
 	public Car() {}
 	public Car(String brand, String color, String type) {
@@ -30,8 +35,27 @@ public class Car {
 	public void setType(String type) {
 		this.type = type;
 	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public boolean isAvailable() {
+		return available;
+	}
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	public Date getFirstReleaseDate() {
+		return firstReleaseDate;
+	}
+	public void setFirstReleaseDate(Date firstReleaseDate) {
+		this.firstReleaseDate = firstReleaseDate;
+	}
+	
 	@Override
 	public String toString() {
-		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + "]";
+		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + ", price=" + price + ", available=" + available + ", first release date=" + firstReleaseDate + "]";
 	}
 }
