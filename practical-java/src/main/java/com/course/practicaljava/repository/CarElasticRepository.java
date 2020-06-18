464927c0ce0d70ed386cfd9a4506c0edad337333
samar <kssrgml@gmail.com>
2020-06-18 10:20:06 +0530

    Cars by pagination

--- practical-java/src/main/java/com/course/practicaljava/repository/CarElasticRepository.java
+++ practical-java/src/main/java/com/course/practicaljava/repository/CarElasticRepository.java
@@ -2,6 +2,8 @@ package com.course.practicaljava.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,9 @@ import com.course.practicaljava.rest.domain.Car;
 public interface CarElasticRepository extends ElasticsearchRepository<Car, String> {
 	
 	public List<Car> findByBrandAndColor(String brand, String color);
+	
+	public List<Car> findByFirstReleaseDateAfter(long date);
+	
+	public Page<Car> findByBrandAndColor(String brand, String color, Pageable pagebale);
 
 }
--- practical-java/src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
+++ practical-java/src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
@@ -1,10 +1,16 @@
 package com.course.practicaljava.rest.controller;
 
 import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -81,6 +87,30 @@ public class CarRestController {
 	public List<Car> findCarsByParam(@RequestParam String brand, 
 			                         @RequestParam String color) {
 		return carElasticRepository.findByBrandAndColor(brand, color);
+	}
+	
+	@GetMapping("/cars/date")
+	public List<Car> findCarByReleaseDateAfter(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date releaseDate) {
+		return carElasticRepository.findByFirstReleaseDateAfter(releaseDate.getTime());
+	}
+	
+	@GetMapping("/cars/page")
+	public List<Car> findCarsByParamPagination(@RequestParam String brand, 
+			                         @RequestParam String color, 
+			                         @RequestParam(defaultValue = "0") int page, 
+			                         @RequestParam(defaultValue = "10") int size) {
+		Pageable pageable = PageRequest.of(page, size);
+		return carElasticRepository.findByBrandAndColor(brand, color, pageable).getContent();
+		
+	}
+	
+	@GetMapping("/cars/page/{brand}/{color}")
+	public List<Car> findCarsByPathPagination(@PathVariable String brand, @PathVariable String color, 
+			                        @RequestParam(defaultValue = "0") int page, 
+			                        @RequestParam(defaultValue = "10") int size) {
+		//Displaying car data by price descending order. 
+		Pageable pageable = PageRequest.of(page, size, Sort.by(Direction.DESC, "price"));
+		return carElasticRepository.findByBrandAndColor(brand, color, pageable).getContent();
 	}
 	
 
