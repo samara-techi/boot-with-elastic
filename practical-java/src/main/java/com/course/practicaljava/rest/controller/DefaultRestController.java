b9a7d92fa01254d9c6ad57c79aaff9fb60c882bd
samar <kssrgml@gmail.com>
2020-06-18 11:29:04 +0530

    Adding data to headers

--- practical-java/src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
+++ practical-java/src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
@@ -1,9 +1,14 @@
 package com.course.practicaljava.rest.controller;
 
 import java.time.LocalTime;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +24,33 @@ public class DefaultRestController {
 	@GetMapping("/time")
 	public String showTime() {
 		return LocalTime.now().toString();
+	}
+	
+	//Working with headers
+	@GetMapping(path="/get-header1")
+	public String header1(@RequestHeader("User-agent") String userAgent, 
+			              @RequestHeader("Practical-java") String practicalJava) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("product version: " + userAgent);
+		sb.append("||");
+		sb.append("About project " + practicalJava);
+		
+		return sb.toString();
+	}
+	
+	@GetMapping(path="/get-header2")
+	public String header2(HttpServletRequest request) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("product version: " + request.getHeader("User-agent"));
+		sb.append("||");
+		sb.append("About project " + request.getHeader("Practical-java"));
+		
+		return sb.toString();
+	}
+	
+	@GetMapping(path="/get-header3")
+	public Map<String, String> header3(@RequestHeader HttpHeaders headers) {
+		return headers.toSingleValueMap();
 	}
 
 }
